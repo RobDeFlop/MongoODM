413a19c3683c73b02603f24e3ed48f834caeda44
RobDeFlop <domeme.ks@gmail.com>
2021-08-10 20:54:59 +0200

    reinit commit

---
+++ Core/MongoConnection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace MongoODM.Core
+{
+    public class MongoConnection
+    {
+        public MongoClient MongoClient = null;
+
+        /// <summary>
+        /// Creates an instance of MongoConnection and establishes the connection
+        /// </summary>
+        /// <param name="host"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="schema"></param>
+        /// <param name="port">Standard port is set to 27017</param>
+        public MongoConnection(string host, string username, string password, string schema, int port = 27017)
+        {
+            
+        }
+
+        /// <summary>
+        /// Creates an instance of MongoConnection and establishes the connection
+        /// </summary>
+        /// <param name="connectionString">Add a connection string</param>
+        /// <param name="schema"></param>
+        public MongoConnection(string connectionString, string schema)
+        {
+            InitWithConnectionString(connectionString);
+            SelectDatabase(schema);
+        }
+        
+        /// <summary>
+        /// Connects to a database with a connection string
+        /// </summary>
+        /// <param name="connectionString"></param>
+        private void InitWithConnectionString(string connectionString)
+        {
+           MongoClient = new MongoClient(connectionString);
+        }
+
+        private void SelectDatabase(string databaseName)
+        {
+            MongoClient.GetDatabase(databaseName);
+        }
+    }
+}
